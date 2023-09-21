e8d0b99f6a6897a65f7d0cd4cb872c2cccc5d306
Corby7 <corbijn.bulsink@hotmail.com>
2023-09-21 16:33:52 +0200

    removed try/finally from connectdatabase #2

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -6,20 +6,14 @@ function connectDatabase() {
     $password = "1234";
     $dbname = "corbijns_webshop";
 
-    try {
-        // Create connection
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        // Check connection
-        if (!$conn) {
-            throw new Exception("Connection failed: " . mysqli_connect_error());
-        }
-        return $conn;
-        echo "Connected successfully";
-    } catch (Exception $e) {
-        //handle the exception
-        echo "Connection failed: " . $e->getmessage();
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+        throw new Exception("Connection failed: " . mysqli_connect_error());
     }
-    
+    return $conn;
+    echo "Connected successfully";
 }
 
 function overwritePassword($email, $newpass) {
