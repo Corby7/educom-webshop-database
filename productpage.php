ad04758a51bddaff247a03604472f065f5793f75
Corby7 <corbijn.bulsink@hotmail.com>
2023-09-22 16:32:25 +0200

    created productpage template #4

---
+++ productpage.php
@@ -0,0 +1,18 @@
+<?php
+
+/** Display the title for the product page. */
+function showProductPageTitle() {
+    echo 'ProductPage';
+}
+
+/** Display the header for the product page. */
+function showProductPageHeader() {
+    echo 'ProductPage';
+}
+
+/** Display the content for the product page. */
+function showProductPageContent() {
+    
+}
+
+?>
