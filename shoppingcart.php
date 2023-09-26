aab004c4327c097bd0e10afe6d321a831e33f4c6
Corby7 <corbijn.bulsink@hotmail.com>
2023-09-26 10:28:15 +0200

    further developed shoppingcart page #5

--- shoppingcart.php
+++ shoppingcart.php
@@ -16,30 +16,45 @@ function showShoppingCartContent($data) {
     <div class="shoppingcart">
         <table>
             <tr>
-                <th>plaatje</th>
-                <th>naam</th>
-                <th>aantal</th>
-                <th>prijs</th>
+                <th></th>
+                <th>Artikel</th>
+                <th>Prijs</th>
+                <th>Aantal</th>
+                <th>Subtotaal</th>
             </tr>';
 
             foreach ($data['products'] as $product) {
                 extract($product);
+                $quantity = $_SESSION['shoppingcart'][$id];
                 
                 echo '
                 <tr>
                     <td><img src="images/' . $filenameimage . '"</td>
                     <td>' . $name . '</td>
-                    <td>placeholder</td>
-                    <td>' . $price . '</td>
+                    <td>€' . $price . '</td>
+                    <td>
+                        <form method="post" action="index.php">
+                            <input type="hidden" name="id" value=' . $id . '>
+                            <input type="hidden" name="page" value="shoppingcart">
+                            <button type="submit" name="action" value="removefromcart">-</button>
+                            ' . $quantity . '
+                            <button type="submit" name="action" value="addtocart">+</button>
+                        </form>
+                    </td>
+                    <td>€' . $subtotal = $price * $quantity . '</td>
                 </tr>';
             }
             
             echo '
             <tr>
-                <th>Subtotaal:</th>
+                <th>Totaal:</th>
             </tr>
         </table>
     </div>';
+}
+
+function showEmptyShoppingCart() {
+    echo 'Uw winkelmandje is leeg.';
 }
 
 ?>
